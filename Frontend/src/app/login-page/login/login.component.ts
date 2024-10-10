4e83b1cc1b2c757921858aaea9fb36fa9387ece2
Christian Kern Pires dos Santos <christiankernp@gmail.com>
2024-10-10 16:19:26 -0300

    Change design of login page

--- Frontend/src/app/login-page/login/login.component.ts
+++ Frontend/src/app/login-page/login/login.component.ts
@@ -8,12 +8,3 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   
 }
-
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('registro');
-const loginBtn = document.getElementById('login');
-
-
-registerBtn.addEventListener('click', () => {container.classList.add("active");})
-
-loginBtn.addEventListener('click', () => {container.classList.remove("active");})
