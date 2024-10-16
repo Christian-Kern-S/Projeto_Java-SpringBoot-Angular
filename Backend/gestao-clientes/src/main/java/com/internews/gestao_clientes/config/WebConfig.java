dfe108a56faf9f91856f4ad437df5cb9a5b3aeaa
Christian Kern Pires dos Santos <christiankernp@gmail.com>
2024-10-16 10:19:34 -0300

    Add config to connect at amazon aws

--- Backend/gestao-clientes/src/main/java/com/internews/gestao_clientes/config/WebConfig.java
+++ Backend/gestao-clientes/src/main/java/com/internews/gestao_clientes/config/WebConfig.java
@@ -1,16 +1,18 @@
 package com.internews.gestao_clientes.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // substitua pelo seu domínio
-                .allowedMethods("*");
+                .allowedOrigins("http://localhost:4200", "http://internews-bucket.s3-website.us-east-2.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
     }
 }
