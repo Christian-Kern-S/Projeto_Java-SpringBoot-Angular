9e97453f3ce945402370ee7785f2a367fc0846c1
Christian-Kern-S <christiankernp@gmai.com>
2025-06-05 17:20:06 -0300

    Adiciona o findById

--- Backend/gestao-clientes/src/main/java/com/internews/gestao_clientes/repositories/UsuarioRepository.java
+++ Backend/gestao-clientes/src/main/java/com/internews/gestao_clientes/repositories/UsuarioRepository.java
@@ -4,7 +4,9 @@ import com.internews.gestao_clientes.models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
     Optional<UsuarioModel> findByUsername(String username);
+    Optional<UsuarioModel> findById(UUID id_user);
 }
